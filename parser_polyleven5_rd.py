d181777e7366e8f19c50829009ab369361d7bef7
Technological-Unemployment <josephjones7@fastmail.fm>
2020-08-18 08:01:22 -0700

    Update parser_polyleven5_rd.py

--- parser_polyleven5_rd.py
+++ parser_polyleven5_rd.py
@@ -12,7 +12,7 @@ import numpy as np
 from io import StringIO
 from nltk.tokenize import word_tokenize
 import polyleven
-PdfPath = r'C:\Users\Computer\Desktop\NLP_Resume'
+pdf_path = r'C:\Users\Computer\Desktop\NLP_Resume'
 OutputPath = r"C:\\Users\\Computer\\Desktop\\output\\"
 
 def poly_ratio(s1, s2):
@@ -27,13 +27,13 @@ with open("C:\\Users\\Computer\\Desktop\\doorway_project\\dedup1.csv") as file1:
     plist = list_names.split('\n')
 ### http://blog.alivate.com.au/poppler-windows/ to add poppler if need be
 ########### https://stackoverflow.com/questions/14640509/python-error-when-importing-image-to-string-from-tesseract
-def convert_pdf(PdfPath, OutputPath):
-    for pdf_file in os.listdir(PdfPath):
+def convert_pdf(pdf_path, OutputPath):
+    for pdf_file in os.listdir(pdf_path):
         # save temp image files in temp dir, delete them after we are finished  fmt="jpeg", jpegopt={"quality": 100,"progressive": True,"optimize": True}
         with tempfile.TemporaryDirectory() as temp_dir:
             # print('created temporary directory', temp_dir)
             # convert pdf to multiple image
-            joined = os.path.join(PdfPath, pdf_file)
+            joined = os.path.join(pdf_path, pdf_file)
             imgs = convert_from_path(joined, dpi=400, thread_count=1, output_folder=temp_dir) ##, poppler_path=r"C:\\Users\\Computer\\poppler-0.68.0_x86\\poppler-0.68.0/bin"
             # find minimum width of images
             min_img_width = min(i.width for i in imgs)
@@ -52,7 +52,7 @@ def convert_pdf(PdfPath, OutputPath):
             newfilename = pdf_file[:-4] + str(i + 1) + '.jpg'
             output_file_name = os.path.join(OutputPath, newfilename)
             merged_image.save(output_file_name)
-convert_pdf(PdfPath, OutputPath)
+convert_pdf(pdf_path, OutputPath)
 # Text extraction from multiple image files using Python
 # Path is given for for 64 bit installer
 pytesseract.pytesseract.tesseract_cmd = "C:/Program Files/Tesseract-OCR/tesseract.exe"
