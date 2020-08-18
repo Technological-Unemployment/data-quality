fa092f68486bc6847a9cf4f984a159d7243b922e
Technological-Unemployment <josephjones7@fastmail.fm>
2020-08-18 08:04:59 -0700

    Update parser_polyleven5_rd.py

--- parser_polyleven5_rd.py
+++ parser_polyleven5_rd.py
@@ -13,7 +13,7 @@ from io import StringIO
 from nltk.tokenize import word_tokenize
 import polyleven
 pdf_path = r'C:\Users\Computer\Desktop\NLP_Resume'
-OutputPath = r"C:\\Users\\Computer\\Desktop\\output\\"
+output_path = r"C:\\Users\\Computer\\Desktop\\output\\"
 
 def poly_ratio(s1, s2):
         dist = polyleven.levenshtein(s1, s2)
@@ -27,7 +27,7 @@ with open("C:\\Users\\Computer\\Desktop\\doorway_project\\dedup1.csv") as file1:
     plist = list_names.split('\n')
 ### http://blog.alivate.com.au/poppler-windows/ to add poppler if need be
 ########### https://stackoverflow.com/questions/14640509/python-error-when-importing-image-to-string-from-tesseract
-def convert_pdf(pdf_path, OutputPath):
+def convert_pdf(pdf_path, output_path):
     for pdf_file in os.listdir(pdf_path):
         # save temp image files in temp dir, delete them after we are finished  fmt="jpeg", jpegopt={"quality": 100,"progressive": True,"optimize": True}
         with tempfile.TemporaryDirectory() as temp_dir:
@@ -50,21 +50,21 @@ def convert_pdf(pdf_path, OutputPath):
                 y += img.height
             # save merged image
             newfilename = pdf_file[:-4] + str(i + 1) + '.jpg'
-            output_file_name = os.path.join(OutputPath, newfilename)
+            output_file_name = os.path.join(output_path, newfilename)
             merged_image.save(output_file_name)
-convert_pdf(pdf_path, OutputPath)
+convert_pdf(pdf_path, output_path)
 # Text extraction from multiple image files using Python
 # Path is given for for 64 bit installer
 pytesseract.pytesseract.tesseract_cmd = "C:/Program Files/Tesseract-OCR/tesseract.exe"
 f = []
 t = []
 document = []
-for root, dirs, filenames in os.walk(OutputPath):
+for root, dirs, filenames in os.walk(output_path):
     for filename in filenames:
         try:
             print(filename)
             f.append(filename)
-            img = Image.open(OutputPath + filename)
+            img = Image.open(output_path + filename)
             document = pytesseract.image_to_string(img, lang='eng')
             t.append(document)
             word_tokens = list(nltk.word_tokenize(document))
